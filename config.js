f0d8c801d1a056eaab046b09bac9974c1cb06198
barco <93612733+Profleide@users.noreply.github.com>
2023-02-10 16:03:49 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4XJF-RXy3JyIlv64M2W1XpSjiT-7YIqg",
+  authDomain: "rwy54ydtg.firebaseapp.com",
+  databaseURL: "https://rwy54ydtg-default-rtdb.firebaseio.com",
+  projectId: "rwy54ydtg",
+  storageBucket: "rwy54ydtg.appspot.com",
+  messagingSenderId: "1087770480395",
+  appId: "1:1087770480395:web:3b4078b03d89e620d3dc0e"
+};
